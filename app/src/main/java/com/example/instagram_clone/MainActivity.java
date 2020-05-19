be57b4e420b304af7a3c32a4d538ae72de5b8942
lalit_kumar <lalitrajput069@gmail.com>
2020-05-19 19:36:54 +0530

    before making Sign up and login scree

--- app/src/main/java/com/example/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/instagram_clone/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.instagram_clone;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
